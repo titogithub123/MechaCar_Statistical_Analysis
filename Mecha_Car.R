b512b3ab6b7aa33e38265e5eb903f0e491d2b6a5
titogithub123 <93171738+titogithub123@users.noreply.github.com>
2022-03-21 17:45:11 -0700

    Mecha_car

--- Mecha_Car.R
+++ Mecha_Car.R
@@ -1,3 +1,5 @@
+# Start Deliverable One
+
 # Mecha Car
 library(dplyr)
 
@@ -16,4 +18,34 @@ summary(mechacar_lm)
 # view R-squared (0.7149033)
 summary(mechacar_lm)$r.square
 
-# En
+# Start Deliverable Two
+
+# Read csv file
+suspension_coil_df <- read.csv(file='./Data/Suspension_Coil.csv')
+
+# Create summary table
+total_summary <- suspension_coil_df %>% 
+  summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), Standard_Deviation = sd(PSI))
+
+total_summary
+
+# Create a lot summary dataframe
+lot_summary <- suspension_coil_df %>% group_by(Manufacturing_Lot) %>% 
+  summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), Standard_Deviation = sd(PSI))
+
+lot_summary
+
+# Deliverable three - t.test at 0.05 level of significance
+# Ho: mu=1500 p-value = 0.06 > 0.05, therefore failed to reject no null hypothesis 
+# Ha: mu<>1500 
+t.test(suspension_coil_df$PSI,mu=1500)
+
+# t.test by lots
+# start with lot 1
+t.test(subset(suspension_coil_df, Manufacturing_Lot=="Lot1")$PSI,mu=1500)
+
+# t.test lot 2
+t.test(subset(suspension_coil_df, Manufacturing_Lot=="Lot2")$PSI,mu=1500)
+
+# t.test lot3
+t.test(subset(suspension_coil_df, Manufacturing_Lot=="Lot3")$PSI,mu=1500)
